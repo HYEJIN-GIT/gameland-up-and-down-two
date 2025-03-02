2ba04af19ae1b4c9cfdad14b9d48b117ba5b8caf
HYEJIN KWON <chahaa0189@gmail.com>
2025-03-02 20:41:52 +0900

    4th commit

--- script.js
+++ script.js
@@ -8,9 +8,12 @@ let random = 0;
 let goBtn = document.getElementById("btn")
 let inputArea = document.getElementById("input-area")
 let upDown = document.getElementById("result-area")
+let chance = 7;
+let chanceArea = document.getElementById("chance") 
+
 
 goBtn.addEventListener("click",go)
-
+inputArea.addEventListener("focus",function(){inputArea.value = ""})
 
 function randomNum () {
     random = Math.floor(Math.random()*50+1)
@@ -21,16 +24,19 @@ console.log(random)
 
 
 function go (){
+    if(inputArea.value == 0){
+        upDown.textContent = "*경고* 값을 입력하세요! !"
+         upDown.style.color ="red"
+        return
+    }
    if(inputArea<1 ||inputArea.value >50){
     upDown.textContent = "1부터 50까지의 숫자를 입력하세용~!"
     upDown.style.color ="red"
     return
    }
-   if(inputArea<1 ||inputArea.value >50){
-    upDown.textContent = "중복된 숫자 입니다!"
-    upDown.style.color ="red"
-    return
-   }
+   
+chance --;
+ chanceArea.textContent = `남은 기회 : ${chance}번`
 
  if(random>inputArea.value){
     upDown.textContent = "Up! 값을 올리세요!"
@@ -43,5 +49,11 @@ upDown.textContent = "♥ 정답입니다 ♥"
 upDown.style.color ="rgb(68, 0, 151)"
  }
 
+if(chance<1){
+    goBtn.disabled = true;
+  
 
 }
+
+}
+
