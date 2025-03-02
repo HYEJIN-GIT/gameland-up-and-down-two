229b32ccba3fc1ab402f6f5d5f55018811322da1
HYEJIN KWON <chahaa0189@gmail.com>
2025-03-02 20:18:23 +0900

    3rd commit

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 let random = 0;
 let goBtn = document.getElementById("btn")
 let inputArea = document.getElementById("input-area")
+let upDown = document.getElementById("result-area")
 
 goBtn.addEventListener("click",go)
 
@@ -21,10 +22,26 @@ console.log(random)
 
 function go (){
    if(inputArea<1 ||inputArea.value >50){
-    console.log(inputArea)
+    upDown.textContent = "1부터 50까지의 숫자를 입력하세용~!"
+    upDown.style.color ="red"
+    return
+   }
+   if(inputArea<1 ||inputArea.value >50){
+    upDown.textContent = "중복된 숫자 입니다!"
+    upDown.style.color ="red"
     return
    }
 
+ if(random>inputArea.value){
+    upDown.textContent = "Up! 값을 올리세요!"
+    
+ }else if(random<inputArea.value){
+upDown.textContent = "Down! 값을 내리세요!!"
+
+ }else{
+upDown.textContent = "♥ 정답입니다 ♥"
+upDown.style.color ="rgb(68, 0, 151)"
+ }
 
 
 }
