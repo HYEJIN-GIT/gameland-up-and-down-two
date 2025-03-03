8e31cabaf086296bbc80a6975539735ebb7a0eb4
HYEJIN KWON <chahaa0189@gmail.com>
2025-03-03 22:08:29 +0900

    7th commit

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//먼저 랜덤 숫자 나오게 설정하기
-//input에 값 넣고 go 버튼 누르면 결과 값 나오게 하기
 
 
 
@@ -10,6 +8,7 @@ let inputArea = document.getElementById("input-area")
 let upDown = document.getElementById("result-area")
 let chance = 7;
 let chanceArea = document.getElementById("chance") 
+let imageSection = document.querySelector(".image-area")
 let distinct = []
 
 goBtn.addEventListener("click",go)
@@ -53,8 +52,10 @@ chance --;
  chanceArea.textContent = `남은 기회 : ${chance}번`
 
  if(random>inputArea.value){
+    imageSection.scr = '/image/IMG_1275.PNG'
     upDown.textContent = "Up! 값을 올리세요!"
     upDown.style.color =" rgb(4, 118, 249)"
+   
    
     
  }else if(random<inputArea.value){
