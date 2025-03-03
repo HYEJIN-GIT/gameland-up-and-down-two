83f377cbb7b9adb8870f5d9157aef8ca8707682b
HYEJIN KWON <chahaa0189@gmail.com>
2025-03-03 21:14:32 +0900

    6th commit

--- script.js
+++ script.js
@@ -16,11 +16,12 @@ goBtn.addEventListener("click",go)
 inputArea.addEventListener("focus",function(){inputArea.value = ""})
 
 
-function enterKey(){
-    if(window.event.Keycode == 13){
-       
+function enterKey() {
+    if (window.event.keyCode == 13) {
+    go()
     }
 }
+
 
 function randomNum () {
     random = Math.floor(Math.random()*50+1)
@@ -54,6 +55,7 @@ chance --;
  if(random>inputArea.value){
     upDown.textContent = "Up! 값을 올리세요!"
     upDown.style.color =" rgb(4, 118, 249)"
+   
     
  }else if(random<inputArea.value){
 upDown.textContent = "Down! 값을 내리세요!!"
@@ -62,6 +64,13 @@ upDown.textContent = "Down! 값을 내리세요!!"
  }else{
 upDown.textContent = "♥ 정답입니다 ♥"
 upDown.style.color ="rgb(68, 0, 151)"
+
+if(chance == 7){
+    chanceArea.textContent = "한 번에 맞추셨습니당! !"
+}else if(chance>0 && chance<7){
+    chanceArea.textContent = `남은 기회 ${chance}번만에 정답 맞추셨습니다 ! ! `  
+}
+
  }
 
  distinct.push(inputArea.value)
@@ -69,8 +78,7 @@ upDown.style.color ="rgb(68, 0, 151)"
 
 if(chance<1){
     goBtn.disabled = true;
-  
-
+    chanceArea.textContent = "꽝 ! 남은 기회가 없습니다! !"
 }
 
 }
